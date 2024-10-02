799e40be4a36a38247eeecdc08b1d72ae4d3e35f
GoldenFealla <telisacaramore@gmail.com>
2024-10-03 03:43:00 +0700

    fix: i am an idiot

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,63 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"time"
 
-	"github.com/goldenfealla/gear-manager/app"
+	"github.com/go-playground/validator/v10"
+	"github.com/jackc/pgx/v5"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+
+	"github.com/goldenfealla/gear-manager/config"
+	"github.com/goldenfealla/gear-manager/internal/repository/postgres"
+	"github.com/goldenfealla/gear-manager/internal/rest"
+	"github.com/goldenfealla/gear-manager/usecase"
 )
 
 func main() {
-	server := app.New()
+	// Loading config
+	c := config.Load()
 
-	err := server.Start()
+	// Connect to database
+	conn, err := pgx.Connect(context.Background(), c.Postgres)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer conn.Close(context.Background())
 
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.CORS())
+	e.Use(middleware.TimeoutWithConfig(middleware.TimeoutConfig{
+		Skipper:      middleware.DefaultSkipper,
+		ErrorMessage: "Request timeout",
+		OnTimeoutRouteErrorHandler: func(err error, c echo.Context) {
+			log.Println(c.Path())
+		},
+		Timeout: 10 * time.Second,
+	}))
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status}\n",
+	}))
+
+	// set up validator
+	v := validator.New(validator.WithRequiredStructEnabled())
+
+	// Build Repository
+	gr := postgres.NewGearRepository(conn)
+
+	// Build Usecase
+	gu := usecase.NewGearUsecase(gr)
+
+	// Build Handler
+	rest.NewUserHandler(e, nil)
+	rest.NewGearHandler(e, gu, v)
+
+	err = e.Start(fmt.Sprintf("%v:%v", c.Host, c.Port))
 	if err != nil {
 		log.Fatalln(err)
 	}
--- domain/user.go
+++ domain/user.go
@@ -1,6 +1,17 @@
 package domain
 
+import "github.com/google/uuid"
+
 type User struct {
-	ID   int64  `json:"id"`
-	Name string `json:"name"`
+	ID       uuid.UUID `json:"id" db:"id"`
+	Username string    `json:"username" db:"username"`
+	Email    string    `json:"email" db:"email"`
+	Password string    `json:"password" db:"password"`
+}
+
+type Profile struct {
+	ID        uuid.UUID `json:"id" db:"id"`
+	FirstName string    `json:"first_name" db:"first_name"`
+	LastName  string    `json:"last_name" db:"last_name"`
+	Phone     string    `json:"phone" db:"phone"`
 }
