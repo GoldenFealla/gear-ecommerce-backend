976883016ccd71592666f9c776eb76d1423ffa44
goldenfealla <telisacaramore@gmail.com>
2024-11-16 09:08:42 +0700

    fix(user): merge profile and user type into one

--- domain/user.go
+++ domain/user.go
@@ -3,17 +3,23 @@ package domain
 import "github.com/google/uuid"
 
 type User struct {
-	ID       uuid.UUID `json:"id" db:"id"`
-	Username string    `json:"username" db:"username"`
-	Email    string    `json:"email" db:"email"`
-	Password string    `json:"password" db:"password"`
-	Verified bool      `json:"verified" db:"verified"`
+	ID        uuid.UUID `json:"id" db:"id"`
+	Username  string    `json:"username" db:"username"`
+	Email     string    `json:"email" db:"email"`
+	FirstName string    `json:"first_name" db:"first_name"`
+	LastName  string    `json:"last_name" db:"last_name"`
+	Phone     string    `json:"phone" db:"phone"`
+	Password  string    `json:"password" db:"password"`
+	Verified  bool      `json:"verified" db:"verified"`
 }
 
 type UserInfo struct {
-	ID       uuid.UUID `json:"id" db:"id"`
-	Username string    `json:"username" db:"username"`
-	Email    string    `json:"email" db:"email"`
+	ID        uuid.UUID `json:"id" db:"id"`
+	Username  string    `json:"username" db:"username"`
+	Email     string    `json:"email" db:"email"`
+	FirstName string    `json:"first_name" db:"first_name"`
+	LastName  string    `json:"last_name" db:"last_name"`
+	Phone     string    `json:"phone" db:"phone"`
 }
 
 type UserCredential struct {
@@ -22,19 +28,15 @@ type UserCredential struct {
 }
 
 type RegisterUserForm struct {
-	Username string `json:"username" validate:"required,gte=6,lte=20"`
-	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required,gte=8,lte=24"`
+	Username  string `json:"username" validate:"required,gte=6,lte=20"`
+	Email     string `json:"email" validate:"required,email"`
+	FirstName string `json:"first_name" db:"first_name" validate:"required,gte=2,lte=30"`
+	LastName  string `json:"last_name" db:"last_name" validate:"required,gte=2,lte=30"`
+	Phone     string `json:"phone" db:"phone" validate:"required"`
+	Password  string `json:"password" validate:"required,gte=8,lte=24"`
 }
 
 type LoginUserForm struct {
 	UsernameOrEmail string `json:"username_or_email" validate:"required"`
 	Password        string `json:"password" validate:"required,gte=8,lte=24"`
-}
-
-type Profile struct {
-	ID        uuid.UUID `json:"id" db:"id"`
-	FirstName string    `json:"first_name" db:"first_name"`
-	LastName  string    `json:"last_name" db:"last_name"`
-	Phone     string    `json:"phone" db:"phone"`
 }
